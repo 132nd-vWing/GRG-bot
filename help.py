02e657b191e82a63e720e3d4c31a6753d090a4e2
132nd.Professor <discord: Professor>
2019-08-09 12:06:48 +0200

    Fixed typo in help.py

--- help.py
+++ help.py
@@ -2,7 +2,7 @@ help_message = """
 Upload an image, and put `!grg` at the beginning of the message.
 I understand the following arguments: `h_pages, v_pages, title, keypad, north, nx, ny, width, scale`.
 Each keyword must be followed by "=" and the value. Multiple keywords must be separated by comma.
-Also, I understand the following falgs: `join`.
+Also, I understand the following flags: `join`.
 Example: `!grg title = AO Charlie, h_pages = 2, ny=10, scale=4.5, width=10, north=4`
 
 For more details, visit https://github.com/132nd-vWing/GRG-bot.
