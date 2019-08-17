27551516a0a8bcce009e935eee0961e7a12b2c5f
132nd.Professor <discord: Professor>
2019-08-17 15:08:38 +0200

    added GPLv3 license

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,19 @@
+# GRG-bot: provide GRGs for maps via a discord bot
+# Copyright © 2019 132nd.Professor
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # bootstrap the musl-version of pdflatex on ubuntu, since the texlive installer doesn't run out-of-the-box on alpine
 # the final image runs on alpine
 FROM ubuntu:disco AS builder
--- help.py
+++ help.py
@@ -1,3 +1,19 @@
+# GRG-bot: provide GRGs for maps via a discord bot
+# Copyright © 2019 132nd.Professor
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 help_message = """
 Upload an image, and put `!grg` at the beginning of the message.
 I understand the following arguments: `h_pages, v_pages, title, keypad, north, nx, ny, width, scale`.
