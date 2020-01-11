ec867f132eb63a95cc4ee60f3b8f0d98e86dbf64
132nd.Professor <discord: Professor>
2020-01-11 22:33:42 +0100

    improved fuzzer, found suspicious test cases

--- unittests/test_parse_args_fuzzer.py
+++ unittests/test_parse_args_fuzzer.py
@@ -1,14 +1,15 @@
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, call
 from unittest.case import TestCase
-import tempfile
+from os import unlink
+from tempfile import NamedTemporaryFile
 
 from main import parse_args
 
 
 class TestParseArgsFuzzer(TestCase):
     def test_basic_call(self):
-        with tempfile.NamedTemporaryFile(delete=False) as fw:
-            for i in range(1000):
+        with NamedTemporaryFile(delete=False) as fw:
+            for i in range(10000):
                 p = Popen(['radamsa'], stdout=PIPE, stdin=PIPE, stderr=PIPE)
                 try:
                     test_args = p.communicate(input='h_pages=2'.encode())[0]
@@ -22,7 +23,20 @@ class TestParseArgsFuzzer(TestCase):
                         fw.write(test_args + '\n'.encode())
                 finally:
                     p.terminate()
-        with open(fw.name, 'r') as fr:
-            print(fr.read())
+        call('sort -u {}'.format(fw.name), shell=True)
+        unlink(fw.name)
         ret = input('Looking good? [N, y] ')
         self.assertEqual(ret.lower().strip(), 'y')
+
+"""
+the following outputs are returned as valid, which constitutes an error: 
+
+=1
+1
+2
+h_pages
+h_pages=
+h_pages=2
+ h_pages=2
+h_pages=2
+"""
